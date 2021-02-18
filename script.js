6b0b716d79935d68019abb34458652a0d046afdf
Glitch (hello-webpage) <none>
2021-02-18 18:08:49 +0000

    🏗🍱 Checkpoint ./page2.html :28239617/3732 ./page22.html:28239617/2932 ./index.html:28239617/5757 ./script.js:28239617/1094 ./style.css:28239617/1165 ./page2.html:28239617/18

--- script.js
+++ script.js
@@ -3,3 +3,32 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+
+function valider() {
+	
+	alert("ok")
+	var nom = document.forms["nom"]["nom"].value;
+	var rep1 = document.forms["q1"]["pays"].value;
+	var rep2 = document.forms["q2"]["poste"].value;
+	var rep3 = document.forms["q3"]["nom_radio"].value;
+	var rep4 = document.forms["q4"]["nom_radio"].value;
+	var rep5 = document.forms["q5"]["nom_radio"].value;
+	var resultat = 0;
+	
+	
+	if (rep1 == "au Danemark") {resultat += 1};
+	
+	if (rep2 == "5") {resultat += 1};
+	
+	if (rep3 == "truc") {resultat += 1};
+	
+	if (rep4 == "truc") {resultat += 1};
+	
+	if (rep5 == "truc") {resultat += 1};
+	
+	alert(resultat);
+	document.getElementaryById("texte").innerHTML =  "Merci pour ta participation" + nom + "! Tu as" + resultat + "bonne/s réponse/s !";
+	
+	
+}
+
