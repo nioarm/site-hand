97c29fcaa599cde8869427f59e688c1728477426
Glitch (hello-webpage) <none>
2021-03-03 02:13:18 +0000

    🍫🏥 Checkpoint ./page2.html:28239617/145 ./script.js:28239617/338

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ console.log("hi");
 function valider() {
 	
 	alert("ok")
-	var nom = document.forms["nom"]["nom"].value;
+	var nom = document.forms["quizz"].value;
+  alert(nom)
 	var rep1 = document.forms["q1"]["pays"].value;
 	var rep2 = document.forms["q2"]["JO"].value;
   var rep3 = document.forms["q3"]["poste"].value;
 	var rep4 = document.forms["q4"]["durée"].value;
-	var rep5 = document.forms["q5"]["nom_radio"].value;
+	var rep5 = document.forms["q5"]["pas"].value;
+  var rep6 = document.forms["q6"]["le"].value;
+  var rep7 = document.forms["q7"]["nom_radio"].value;
 	var resultat = 0;
 	
 	
@@ -24,7 +27,11 @@ function valider() {
 	
 	if (rep4 == "60 minutes") {resultat += 1};
 	
-	if (rep5 == "truc") {resultat += 1};
+	if (rep5 == "3") {resultat += 1};
+  
+  if (rep5 == "truc") {resultat += 1};
+  
+  if (rep5 == "truc") {resultat += 1};
 	
 	alert(resultat);
 	document.getElementaryById("texte").innerHTML =  "Merci pour ta participation" + nom + "! Tu as" + resultat + "bonne/s réponse/s !";
