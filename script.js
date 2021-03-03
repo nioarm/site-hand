6cee7d3749202af91b2c2d1ecc224856d6803fff
Glitch (hello-webpage) <none>
2021-03-03 15:25:00 +0000

    🏣🛥 Checkpoint ./script.js:28239617/1645

--- script.js
+++ script.js
@@ -31,9 +31,9 @@ function valider() {
   if (rep6 == "6 coupes") {resultat += 1};
   
   if (rep7 == "médaille d'argent") {resultat += 1};
+  alert("Merci pour ta participation" + nom + "! Tu as" + resultat + "bonne/s réponse/s !")
+	document.getElementaryById("texte").innerHTML =  "Merci pour ta participation \u0020 |" + nom + "! Tu as" + resultat + " bonne/s réponse/s !";
 	
-	document.getElementaryById("texte").innerHTML =  "Merci pour ta participation" + nom + "! Tu as" + resultat + "bonne/s réponse/s !";
-	alert("Merci pour ta participation" + nom + "! Tu as" + resultat + "bonne/s réponse/s !")
 	
 }
 
