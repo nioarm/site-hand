2aee8786acc338b107569e8d0385d6efc219f7f9
Glitch (hello-webpage) <none>
2021-03-03 15:58:46 +0000

    📺🍁 Checkpoint ./script.js:28239617/234

--- script.js
+++ script.js
@@ -32,8 +32,7 @@ function valider() {
   
   if (rep7 == "médaille d'argent") {resultat += 1};
   alert("Merci pour ta participation " + nom + " ! Tu as " + resultat + " bonne/s réponse/s !")
-	document.getElementaryById("texte").innerHTML = "Merci pour ta participation " + nom + "! Tu as" + resultat + " bonne/s réponse/s !"
-	
-	
+	document.getElementaryById("texte").innerHTML = "Merci pour ta participation " + nom + "! Tu as" + resultat + " bonne/s réponse/s !";
+	alert("ok")
 }
 
