499b8563a456e2c0fc379db5a021430235e185ca
Glitch (hello-webpage) <none>
2021-03-03 02:58:14 +0000

    👕🏭 Checkpoint ./script.js:28239617/144 ./page2.html:28239617/608

--- script.js
+++ script.js
@@ -8,14 +8,12 @@ function valider() {
 	
 	alert("ok")
 	var nom = document.forms["nom"]["nom"].value;
-  alert(nom)
 	var rep1 = document.forms["q1"]["pays"].value;
 	var rep2 = document.forms["q2"]["JO"].value;
   var rep3 = document.forms["q3"]["poste"].value;
 	var rep4 = document.forms["q4"]["durée"].value;
 	var rep5 = document.forms["q5"]["pas"].value;
   var rep6 = document.forms["q6"]["cmonde"].value;
-  alert(rep6)
   var rep7 = document.forms["q7"]["nom_radio"].value;
 	var resultat = 0;
 	
@@ -31,7 +29,7 @@ function valider() {
 	if (rep5 == "3") {resultat += 1};
   
   if (rep6 == "6 coupes") {resultat += 1};
-  alert(resultat)
+  
   if (rep5 == "truc") {resultat += 1};
 	
 	alert(resultat);
