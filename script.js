532d9d1be24295a84745cd3ebd4835005d9b12be
Glitch (hello-webpage) <none>
2021-03-22 21:07:41 +0000

    🏋🚂 Checkpoint ./script.js:28239617/89

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ console.log("hi");
 function valider() {
 	
   if (confirm('voulez vous valider vos réponses ?')){
-  }else{
-  return 
+  } else{
+  return;
   }
   
 	var nom = document.forms["nom"]["nom"].value;
@@ -24,35 +24,43 @@ function valider() {
 	
 	if (rep1 == "au Danemark") {
     resultat += 1;
-  } else (alert('La 1ère réponse est fausse, le handball a été inventé au Danemark.'));
+  } else {
+    (alert('La 1ère réponse est fausse, le handball a été inventé au Danemark.'));}
   
 	if (rep2 == "en 1928") {
     resultat += 1;
-  } else (alert('La 2ème réponse est fausse, le handball est apparu pour la première fois aux J.O. en 1928.'));
+  } else {
+    (alert('La 2ème réponse est fausse, le handball est apparu pour la première fois aux J.O. en 1928.'));}
   
 	if (rep3 == "5") {
     resultat += 1;
-  } else (alert('La 3ème réponse est fausse, il existe 5 postes au handball.'));
+  } else {
+    (alert('La 3ème réponse est fausse, il existe 5 postes au handball.'));}
   
 	if (rep4 == "60 minutes") {
     resultat += 1;
-  } else (alert('La 4ème réponse est fausse, un match dure 60 minutes.'));
+  } else {
+    (alert('La 4ème réponse est fausse, un match dure 60 minutes.'));}
   
 	if (rep5 == "3") {
     resultat += 1;
-  } else (alert('La 5ème réponse est fausse, on peut faire 3 pas avec le ballon sans dribbler.'));
+  } else {
+    (alert('La 5ème réponse est fausse, on peut faire 3 pas avec le ballon sans dribbler.'));}
   
   if (rep6 == "6 coupes") {
     resultat += 1;
-  } else (alert('La 6ème réponse est fausse, l équipe de France masculine a remportée 6 coupes du monde.'));
+  } else {
+    (alert('La 6ème réponse est fausse, l équipe de France masculine a remportée 6 coupes du monde.'));}
   
   if (rep7 == "médaille d'argent") {
     resultat += 1;
-  } else (alert('La 7ème réponse est fausse, l équipe de France féminine a remportée la médaille d argent aux J.O. en 2016.'));
+  } else {
+    (alert('La 7ème réponse est fausse, l équipe de France féminine a remportée la médaille d argent aux J.O. en 2016.'));}
   
   if (rep8 == "en Egypte") {
     resultat += 1;
-  } else (alert('La 8ème réponse est fausse, le dernier championnat du monde masculin s est déroulé en Egypte.'));
+  } else {
+    (alert('La 8ème réponse est fausse, le dernier championnat du monde masculin s est déroulé en Egypte.'));}
   
 	document.getElementById("texte").innerHTML = "Merci de votre participation " + nom + " ! Vous avez " + resultat + " bonne/s réponse/s sur 8 !";
 		
